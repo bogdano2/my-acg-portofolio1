14b359087e45d828d90301aabd47c252b0858774
bogdano2 <bogdan@technocampus.com>
2020-05-27 13:16:46 -0500

    First commit post lab

---
+++ js/config.js
@@ -0,0 +1,7 @@
+var configConstants = {
+    auth0: {
+        domain: 'bog-acg.auth0.com',
+        clientId: '0mVqda66O7tpH5Zc2S93FjxYyQQuV7m5'
+    },
+    apiBaseUrl: 'https://z9sl0u5w18.execute-api.us-east-1.amazonaws.com/dev'
+};
